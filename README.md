8dd56b7339fe370f64fa712d54cec6f5aabe0094
TarunVisva07 <tarunvisva78@gmail.com>
2021-07-31 17:57:28 +0530

    ReadMe updated

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Desktop-cleaner
+
+In this pandemic situation where most of our daily events are happening virtually , most of us use a computer/laptop everyday for online classes,work from home etc..
+As a result we end up with a lot of files in our desktop which makes it look a little rubbish. Being an Engineering student 
+my desktop looked the same, flooded with lots of pdfs,books,ppts,images etc.. scattered here and there. Creating a folder for different types of files and moving
+files into the respective folders is seriously a tiresome task. A simple idea sparked in my mind to assign this tiresome task to the computer and this python program is 
+an implementation of my idea.
+
+This is an interactive python program which cleans the desktop (or preferrably any other directory) by moving the files of 
+same type(extension) to a new directory or a directory that already exists. It also has various other functionalities.
+
+The program gets the extension and the folder name as inputs and moves the files of that particular extension present 
+in desktop into the folder specified by user.This program has been  developed using os,shutil,and re modules of python.
+Upon initiating the program, it runs an infinite while loop where it asks for the inputs and finishes the job.
+
+This program is implemented as a class DesktopInterface which has the following methods:
+
+(i)__init__(desktop) - Constructor of the class which gets the path to desktop (or preferrably any other directory) as input and stores the files 
+in the directory 
+
+(ii)show_files(ext) - Prints all the files of the particular extension present in desktop
+
+(iii)show_directories() - Prints all directories present in desktop
+
+(iv)get_files_by__extension() - Returns the dictionary that contains all the files in desktop classified according to their extensions and prints the same
+
+(v)classify(ext,dir) - Gets extension and directory as input and moves the files of that particular extension to the directory specified.
+If the directory does not exist, the method creates a new directory.
+
+(vi)delete_dir(dir) - Just an implementation of shutil.rmtree function which deletes a directory 
---
+++ desktop cleaner.py
@@ -0,0 +1,77 @@
+import os
+import re
+import shutil
+import time
+
+print("Press Ctrl + C to stop any program .. :) ../..")
+
+
+class DesktopInterface:
+
+    def __init__(self, desktop):
+        self.desktop = desktop
+        self.files = list()
+        for file in os.listdir(desktop):
+            if "." in file:
+                self.files.append(file)
+
+    def show_files(self, ext):
+        for file in self.files:
+            if file.endswith("." + ext):
+                print(file)
+
+    def show_directories(self):
+        i = 1
+        for file in os.listdir(self.desktop):
+            if "." not in file:
+                print("{}. {}".format(i, file))
+                i += 1
+
+    def get_files_by_extension(self):
+        li = [re.findall("\.(.+)", file)[0] for file in self.files]
+        li = list(set(li))
+        li.sort()
+        di = dict()
+        print("\n\n\nFILES BY EXTENSION : ")
+        for x in li:
+            i = 1
+            print(x.upper(), ":")
+            di[x.upper()] = list()
+            for file in self.files:
+                if file.endswith("." + x):
+                    di[x.upper()].append(file)
+                    print("{}. {}".format(i, file))
+                    i += 1
+        return di
+
+    def classify(self, ext, _dir):
+        dst = os.path.join(self.desktop, _dir)
+        try:
+            os.mkdir(dst)
+            print("New directory created..")
+            time.sleep(2)
+        except FileExistsError:
+            print("Directory already exists .... Moving there ")
+            time.sleep(2)
+            pass
+        for file in self.files:
+            if file.endswith("." + ext):
+                src = os.path.join(self.desktop, file)
+                try:
+                    shutil.move(src, dst)
+                    print("{} moved to {} successfully".format(file, _dir))
+                except shutil.Error as e:
+                    print("Unable to move {} due to {} error".format(file, e))
+                    pass
+                time.sleep(1.5)
+
+    def delete_dir(self, _dir):
+        shutil.rmtree(os.path.join(self.desktop, _dir))
+        print("The directory {} has been deleted successfully ...".format(_dir))
+
+
+interface = DesktopInterface("C://users//tarun//desktop")
+interface.get_files_by_extension()
+while True:
+    interface.classify(input("Enter extension"),input("Enter directory"))
+    
